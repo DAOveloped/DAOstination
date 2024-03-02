ac9df40d13b63db1e5f287f2eb9624c7904921cc
Michael <enjoyweaver@gmail.com>
2024-03-02 11:18:24 -0600

    daostination page

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,8 @@ const navigation = [
 export default function Home() {
   // Function to generate random color
   const generateRandomColor = () => {
-    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+    const color = Math.floor(Math.random() * 16777215).toString(16);
+    return `#${color.padStart(6, "0")}`;
   };
 
   // State to store random text gradient
