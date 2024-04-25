c12d9e5f1abfa569af261a1d3c1a33fb52eab9b5
Michael <enjoyweaver@gmail.com>
2024-04-25 18:50:52 -0500

    image correction

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,13 +3,15 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="py-4 text-center text-white text-xs">
-      Built with AO{" "}
+      Built with{" "}
       <a
         href="https://ao.arweave.dev/"
         target="_blank"
         rel="noopener noreferrer"
         className="text-white hover:text-gray-900"
-      ></a>{" "}
+      >
+        AO
+      </a>{" "}
       on{" "}
       <a
         href="https://arweave.org"
