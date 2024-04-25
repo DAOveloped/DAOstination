a87e93b93328ca20bd1317a9622b339138f72401
Michael <enjoyweaver@gmail.com>
2024-04-25 14:24:53 -0500

    slogan and images

--- app/page.tsx
+++ app/page.tsx
@@ -58,13 +58,13 @@ export default function Home() {
           WebkitTextFillColor: "transparent",
         }} // Apply random text gradient
       >
-        DAOstination DAO
+        da*stination
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          Where is your next DAOstination?
+          stay, work, travel - together
         </h2>
       </div>
     </div>
