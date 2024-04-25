06876f2bd6183a9cb0518ff5960bafffeb5b81bc
Michael <enjoyweaver@gmail.com>
2024-04-25 18:45:48 -0500

    mobile logo size

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import React, { useState, useEffect } from "react";
 import Footer from "./components/footer";
-import styles from "./styles.module.css";
+import "./styles.module.css";
 
 const navigation = [
   { name: "About", href: "/about" },
