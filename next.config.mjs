5f4d51f0103b438b34a762c3a8f1c12925548a64
Michael <enjoyweaver@gmail.com>
2024-04-29 20:50:16 -0500

    vercel error life

--- next.config.mjs
+++ next.config.mjs
@@ -7,15 +7,6 @@ const nextConfig = {
   experimental: {
     mdxRs: true,
   },
-  typescript: {
-    // Custom TypeScript configuration to suppress errors
-    // Suppress errors related to the specific TypeScript error codes you encounter
-    ignore: [
-      // Example: Suppress error TSXXXXX
-      // 'TSXXXXX',
-      // Add more error codes to ignore as needed
-    ],
-  },
 };
 
 export default withContentlayer(nextConfig);
