1b1dd77dc55532423aa1e8826d9db66cefcde942
Michael <enjoyweaver@gmail.com>
2024-04-29 20:32:48 -0500

    vercel errors again

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // Custom TypeScript configuration
-  typescript: {
-    // Override TypeScript configuration options
-    ignoreBuildErrors: true, // Ignore TypeScript errors during the build process
-  },
-};
--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,19 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		mdxRs: true,
-	},
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    mdxRs: true,
+  },
+  typescript: {
+    // Custom TypeScript configuration to suppress errors
+    // Suppress errors related to the specific TypeScript error codes you encounter
+    ignore: [
+      // Example: Suppress error TSXXXXX
+      // 'TSXXXXX',
+      // Add more error codes to ignore as needed
+    ],
+  },
 };
 
 export default withContentlayer(nextConfig);
